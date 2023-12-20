dc08c0cbcc52277dd14773e88895a78850d525ea
Coded Pro <78369018+codedpro@users.noreply.github.com>
2023-12-20 02:11:45 -0800

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  serverRuntimeConfig: {
+    secret:
+      "ASfa-sf9iasffak2kkkkkkasf@@@@-aF0=1021fiasfjasfkasfk@21231240!+R)KAS+Fj=asfjA+SF)JK!+@",
+  },
+  reactStrictMode: false,
+  swcMinify: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  publicRuntimeConfig: {
+    apiUrl: process.env.NODE_ENV === 'development'
+        ? 'http://localhost:27888/api' // development api
+        : 'http://play.madgames.ir:27888/api' // production api
+}
+};
+
+module.exports = nextConfig;
